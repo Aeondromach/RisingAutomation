286e3870b0582299134fac555c4e0b4110545276
landonh2026 <147776242+landonh2026@users.noreply.github.com>
2025-01-15 10:42:10 -0700

    Move compress

--- assets/images/compress.py
+++ assets/images/compress.py
@@ -3,19 +3,27 @@ import os
 
 RESIZE = True
 REMOVE = False
+IMAGE_EXTENSIONS_TO_COMPRESS = [".jpeg", ".jpg", ".png", ".webp"]
 
 directory = os.path.dirname(__file__)
 os.chdir(directory)
-files = os.listdir()
+# files = os.listdir()
 images = []
 
-image_extensions_to_compress = [".jpeg", ".jpg", ".png", ".webp"]
-
-for file in files:
-    for extension in image_extensions_to_compress:
+def file_allowed(file: str) -> bool:
+    for extension in IMAGE_EXTENSIONS_TO_COMPRESS:
         if file.endswith(extension):
+            return True
+    return False
+
+for root, dirs, files in os.walk("."):
+    path = root.split(os.sep)
+    for file in files:
+        if file_allowed(file):
             images.append(file)
-            break
+
+print(images)
+# exit()
 
 for image in images:
     save_path = "".join(image.split(".")[:-1]) + ".webp"
