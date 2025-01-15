0be2ed68cd6b46b3f57412e7860d3eff4d246f60
landonh2026 <147776242+landonh2026@users.noreply.github.com>
2025-01-15 10:35:33 -0700

    Update compress.py

--- assets/compress.py
+++ assets/compress.py
@@ -2,31 +2,30 @@ from PIL import Image
 import os
 
 RESIZE = True
-REMOVE = True
+REMOVE = False
 
 directory = os.path.dirname(__file__)
 os.chdir(directory)
 files = os.listdir()
 images = []
 
-img_extension = ".jpeg"
+image_extensions_to_compress = [".jpeg", ".jpg", ".png", ".webp"]
 
 for file in files:
-    if file.endswith(img_extension):
-        images.append(file)
+    for extension in image_extensions_to_compress:
+        if file.endswith(extension):
+            images.append(file)
+            break
 
-for thisImage in images:
-    image_dir = thisImage
-    save_path = thisImage.split(img_extension)[0] + ".webp"
+for image in images:
+    save_path = "".join(image.split(".")[:-1]) + ".webp"
     
     print(save_path)
     
-    image = Image.open(image_dir)
+    image = Image.open(image)
     image = image.convert('RGB')
     
-    if RESIZE:
-        image.thumbnail((1920, 1080), Image.LANCZOS)
-    if REMOVE:
-        os.remove(image_dir)
+    if RESIZE: image.thumbnail((1920, 1080), Image.LANCZOS)
+    if REMOVE: os.remove(image)
 
     image.save(save_path, 'webp', optimize = True, quality = 70)
