e4792909769d94cb07788d9d8cbb5545011c0fd4
landonh2026 <147776242+landonh2026@users.noreply.github.com>
2025-01-15 14:05:44 -0700

    dynamic object position for each video

--- scripts/hero-video.js
+++ scripts/hero-video.js
@@ -1,5 +1,6 @@
 const heroVideo = document.querySelector('.hero-image.video');
 const videos = heroVideo.getAttribute("data-video-urls").split(' ');
+const videoPositions = heroVideo.getAttribute("data-video-positions").split(' ');
 
 const altHeroVideo = document.createElement("video");
 altHeroVideo.classList.add("hero-image");
@@ -10,44 +11,51 @@ heroVideo.parentElement.insertBefore(altHeroVideo, heroVideo);
 let currentVideo = 0;
 let altActive = false;
 
+// update the video
+// (var for debugging purposes)
 var updateVideo = () => {
     let activeVideo = altActive ? altHeroVideo : heroVideo;
     let inactiveVideo = altActive ? heroVideo : altHeroVideo;
     altActive = !altActive;
 
+    // swap the videos
     inactiveVideo.classList.remove("hidden");
     activeVideo.classList.add("hidden");
 
+    // pause the current video and play the next one
     // activeVideo.pause();
     inactiveVideo.play();
 
+    // set the next video that loads in the background
     setTimeout(() => {
         currentVideo = (currentVideo + 1) % videos.length;
         activeVideo.src = videos[currentVideo];
+        activeVideo.style.objectPosition = "0 " + videoPositions[currentVideo];
         activeVideo.load();
     }, 750);
 }
 
+// on time update listener
 const onTimeUpdate = (event) => {
     let video = altActive ? altHeroVideo : heroVideo;
     
+    // for some reason initially comes up as some value less than 1
     if (video.duration < 1) return;
     
+    // if we are within the last 2 seconds of the video, go to the next video
     if (video.currentTime > video.duration - 2) updateVideo();
 }
 
+// set the current video
 heroVideo.src = videos[currentVideo];
+heroVideo.style.objectPosition = "0 " + videoPositions[currentVideo];
 heroVideo.play();
 
+// set next video
 currentVideo = (currentVideo + 1) % videos.length;
 altHeroVideo.src = videos[currentVideo];
+altHeroVideo.style.objectPosition = "0 " + videoPositions[currentVideo];
 
-// heroVideo.addEventListener('ended', updateVideo);
-// altHeroVideo.addEventListener('ended', updateVideo);
-
+// set listener for time updates
 heroVideo.addEventListener("timeupdate", onTimeUpdate);
 altHeroVideo.addEventListener("timeupdate", onTimeUpdate);
-
-// setInterval(() => {
-//     console.log(heroVideo.currentTime, altHeroVideo.currentTime);
-// }, 500);
